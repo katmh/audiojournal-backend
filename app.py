ed5aee3431196eb33a2faa4df2d0d0cba643cffc
Katherine Huang <hello@katmh.com>
2019-10-26 14:40:09 -0400

    Airtable

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask, request
+from airtable import Airtable
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return request.args
+        airtable = Airtable('appnHKrgvz8qkq4ur', 'Entries', api_key = os.environ['AIRTABLE_API_KEY'])
+
+        airtable.insert({'Title': request.args['title']})
+
+        return airtable.get_all()[-1]
     else:
         return 'Hello, World!'
